57e979c67e819870e279665ae668128fb0ad3d9b
tterzziew <thegamer2235@gmail.com>
2018-03-08 23:57:27 +0200

    Added solution for Problem 2

---
+++ intro-and- basic-syntax/Exercise/Rectangle Area.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double rectangleArea = w * h;
+
+            Console.WriteLine($"{rectangleArea:f2}");
+        }
+    }
+}
