af2714c7054c178fd639fbf24d055bdca70e8e79
Nikolay Grozdanov <nagrozdanov@gmail.com>
2018-03-08 20:13:25 +0200

    added problem one solution

---
+++ intro-and- basic-syntax/Exercise/Debit Card Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Programing_Fundamentals_Exersises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"{a:D4} {b:D4} {c:D4} {d:D4}");
+
+
+        }
+    }
+}
