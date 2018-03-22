b050a864bedc12e7032f292ab7a666f42999d7b1
Max <maxkaft@gmail.com>
2018-03-22 17:51:37 +0200

    Added solution to problem3.

---
+++ intro-and- basic-syntax/Exercise/milesToKm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _03.Miles_to_KM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double km = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:F2}", km * 1.60934);    
+        }
+    }
+}
