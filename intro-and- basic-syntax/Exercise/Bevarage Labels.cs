5a99576d1aed27a54aed41a852714cff82195780
Max <maxkaft@gmail.com>
2018-03-22 18:07:04 +0200

    Changed var name in problem 4.

--- intro-and- basic-syntax/Exercise/Bevarage Labels.cs
+++ intro-and- basic-syntax/Exercise/Bevarage Labels.cs
@@ -4,9 +4,9 @@ namespace Beverage_Labels
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            string name = Console.ReadLine();
+            string nameOfBeverage = Console.ReadLine();
             double volume = double.Parse(Console.ReadLine());
             double energyContentPer100 = double.Parse(Console.ReadLine());
             double sugarContentPer100 = double.Parse(Console.ReadLine());
@@ -14,7 +14,7 @@ namespace Beverage_Labels
             double energyContentTotal = (volume / 100) * energyContentPer100;
             double sugarContentTotal = (volume / 100) * sugarContentPer100;
 
-            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{volume}ml {nameOfBeverage}:");
             Console.WriteLine($"{energyContentTotal}kcal, {sugarContentTotal}g sugars");
 
         }
