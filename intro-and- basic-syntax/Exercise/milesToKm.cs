38920df8d962d6a60667f843fdf4faeead2d96e2
tterzziew <Yavormoyseev@gmail.com>
2018-03-22 18:17:47 +0200

    Confirmed solution for problem 3

--- intro-and- basic-syntax/Exercise/milesToKm.cs
+++ intro-and- basic-syntax/Exercise/milesToKm.cs
@@ -4,6 +4,7 @@ namespace _03.Miles_to_KM
 {
     class Program
     {
+		Confirmed by moyseev
         static void Main(string[] args)
         {
             double distance = double.Parse(Console.ReadLine());
