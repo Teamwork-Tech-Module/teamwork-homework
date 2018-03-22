fdd387cf94b96089f1cf5cecb5f35874d3fb8821
tterzziew <Yavormoyseev@gmail.com>
2018-03-22 18:08:55 +0200

    Changed var name in problem 1

--- intro-and- basic-syntax/Exercise/Debit Card Number.cs
+++ intro-and- basic-syntax/Exercise/Debit Card Number.cs
@@ -6,13 +6,13 @@ namespace Programing_Fundamentals_Exersises
     {
         static void Main(string[] args)
         {
-            int a = int.Parse(Console.ReadLine());
+            int first = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             int c = int.Parse(Console.ReadLine());
             int d = int.Parse(Console.ReadLine());
 
 
-            Console.WriteLine($"{a:D4} {b:D4} {c:D4} {d:D4}");
+            Console.WriteLine($"{first:D4} {b:D4} {c:D4} {d:D4}");
 
 
         }
