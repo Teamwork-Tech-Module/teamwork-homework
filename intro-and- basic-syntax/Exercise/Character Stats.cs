7f445dca0377141d68ee8558c1bbf866bc9c56c4
tterzziew <thegamer2235@gmail.com>
2018-03-22 18:12:47 +0200

    Changed var name in problem 5

--- intro-and- basic-syntax/Exercise/Character Stats.cs
+++ intro-and- basic-syntax/Exercise/Character Stats.cs
@@ -6,13 +6,13 @@ namespace Character_Stats
     {
         static void Main(string[] args)
         {
-            string charName = Console.ReadLine();
+            string nameOfChar = Console.ReadLine();
             int currentHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Name: {charName}");
+            Console.WriteLine($"Name: {nameOfChar}");
             Console.WriteLine($"Health: {new string('|', 1)}{new string('|', currentHealth)}{new string('.', maxHealth - currentHealth)}{new string('|', 1)}");
             Console.WriteLine($"Energy: {new string('|', 1)}{ new string('|', currentEnergy)}{new string('.', maxEnergy - currentEnergy)}{new string('|', 1)}");
         }
